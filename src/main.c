40324f6f443d0da51d8712f3e1403a527ee5c48b
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2023-02-26 15:13:43 +0100

    Add LEDs animation effect

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 LOG_MODULE_REGISTER(pio_zephyr_demo, LOG_LEVEL_DBG);
 
 /* Blink delay in milliseconds */
-#define SLEEP_TIME_MS (1000)
+#define BLINK_DELAY_MS (100)
 
 /* The devicetree node identifiers for the "led0" "led1" and "led2" aliases */
 #define LED0_NODE DT_ALIAS(led0)
@@ -12,9 +12,9 @@ LOG_MODULE_REGISTER(pio_zephyr_demo, LOG_LEVEL_DBG);
 #define LED2_NODE DT_ALIAS(led2)
 
 /* LED GPIO information specified in devicetree */
-static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec greenLED = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec blueLED = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec redLED = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 
 void main(void)
 {
@@ -23,17 +23,17 @@ void main(void)
   LOG_INF("========== PlatformIO Zephyr demo ==========");
   LOG_INF("============================================");
 
-  gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
-  gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
-  gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+  gpio_pin_configure_dt(&greenLED, GPIO_OUTPUT_ACTIVE);
+  gpio_pin_configure_dt(&blueLED, GPIO_OUTPUT_ACTIVE);
+  gpio_pin_configure_dt(&redLED, GPIO_OUTPUT_ACTIVE);
 
-  gpio_pin_toggle_dt(&led1);
   while(1)
   {
-    gpio_pin_toggle_dt(&led0);
-    gpio_pin_toggle_dt(&led1);
-    gpio_pin_toggle_dt(&led2);
-
-    k_msleep(SLEEP_TIME_MS);
+    gpio_pin_toggle_dt(&greenLED);
+    k_msleep(BLINK_DELAY_MS);
+    gpio_pin_toggle_dt(&blueLED);
+    k_msleep(BLINK_DELAY_MS);
+    gpio_pin_toggle_dt(&redLED);
+    k_msleep(BLINK_DELAY_MS);
   }
 }
