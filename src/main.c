907d95bf03c87229051b0dd2608e175387bb6c44
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2023-04-16 17:27:50 +0200

    Add mqtt subscribe + minor changes

--- src/main.c
+++ src/main.c
@@ -32,9 +32,9 @@
 /* Button callback function */
 static void _button_pressed_cb(const struct device *, struct gpio_callback *, uint32_t);
 /* DHCP client callback function */
-static void _dhcpv4_handler(struct net_mgmt_event_callback *, uint32_t, struct net_if *);
+static void _dhcpv4_event_handler(struct net_mgmt_event_callback *, uint32_t, struct net_if *);
 /* MQTT client callback function */
-static void _mqtt_evt_handler(struct mqtt_client *, const struct mqtt_evt *);
+static void _mqtt_event_handler(struct mqtt_client *, const struct mqtt_evt *);
 
 /*-----------------------------------------------------------------------------------------------*/
 /* Private variables                                                                             */
@@ -51,7 +51,7 @@ static struct gpio_callback buttonCbData = {0};
 static struct net_mgmt_event_callback netMgmtEvtCb = {0};
 /* Got IP address flag */
 static bool gotIP = false;
-/* Buffers for MQTT client */
+/* Work buffers for MQTT client */
 static uint8_t rxBuffer[256] = {0};
 static uint8_t txBuffer[256] = {0};
 /* MQTT client context */
@@ -60,6 +60,9 @@ static struct mqtt_client clientCtx = {0};
 static struct sockaddr_storage broker = {0};
 /* MQTT connection flag */
 static bool mqttConnected = false;
+/* Received MQTT message buffer */
+static uint8_t mqttReceivedMessage[32] = {0};
+static int mqttReceivedMessageLength = 0;
 
 /*-----------------------------------------------------------------------------------------------*/
 /* Exported functions                                                                            */
@@ -70,6 +73,8 @@ void main(void)
   struct sockaddr_in *broker4 = NULL;
   struct pollfd fds[1] = {0};
   struct mqtt_publish_param publishParams;
+  static struct mqtt_topic subscribeTopic;
+  static struct mqtt_subscription_list subscribeTopicsList;
 
   printk("============================================\r\n");
   printk("========== PlatformIO Zephyr demo ==========\r\n");
@@ -94,7 +99,7 @@ void main(void)
   printk("Setting up button at %s pin %d\r\n", button.port->name, button.pin);
 
   /* Start DHCP client */
-  net_mgmt_init_event_callback(&netMgmtEvtCb, _dhcpv4_handler, NET_EVENT_IPV4_ADDR_ADD);
+  net_mgmt_init_event_callback(&netMgmtEvtCb, _dhcpv4_event_handler, NET_EVENT_IPV4_ADDR_ADD);
   net_mgmt_add_event_callback(&netMgmtEvtCb);
   netIf = net_if_get_default();
   net_dhcpv4_start(netIf);
@@ -118,9 +123,9 @@ void main(void)
 
   /* MQTT client configuration */
   clientCtx.broker = &broker;
-  clientCtx.evt_cb = _mqtt_evt_handler;
-  clientCtx.client_id.utf8 = (uint8_t *)"zephyr_mqtt_client";
-  clientCtx.client_id.size = sizeof("zephyr_mqtt_client") - 1;
+  clientCtx.evt_cb = _mqtt_event_handler;
+  clientCtx.client_id.utf8 = (uint8_t *)"nucleo-f767zi";
+  clientCtx.client_id.size = sizeof("nucleo-f767zi") - 1;
   clientCtx.password = NULL;
   clientCtx.user_name = NULL;
   clientCtx.protocol_version = MQTT_VERSION_3_1_1;
@@ -146,20 +151,26 @@ void main(void)
     zsock_poll(fds, 1, 500);
     mqtt_input(&clientCtx);
   }
-  printk("\r\n");
 
-  /* Simulate LEDs racing effect */
+  /* Subscribe to MQTT topic */
+  subscribeTopic.topic.utf8 = (uint8_t *)"zephyr/test";
+  subscribeTopic.topic.size = strlen(subscribeTopic.topic.utf8);
+  subscribeTopicsList.list = &subscribeTopic;
+  subscribeTopicsList.list_count = 1U;
+  subscribeTopicsList.message_id = 1U;
+  if(0 == mqtt_subscribe(&clientCtx, &subscribeTopicsList))
+  {
+    printk("MQTT client subscribed to topic\r\n");
+  }
+  else
+  {
+    printk("MQTT client failed to subscribe to topic\r\n");
+  }
+
   while(1)
   {
-    gpio_pin_toggle_dt(&greenLED);
-    k_msleep(BLINK_DELAY_MS);
-    gpio_pin_toggle_dt(&blueLED);
-    k_msleep(BLINK_DELAY_MS);
-    gpio_pin_toggle_dt(&redLED);
-    k_msleep(BLINK_DELAY_MS);
-
-    /* Publish MQTT message */
-    publishParams.message.topic.qos = MQTT_QOS_1_AT_LEAST_ONCE;
+    /* Publish MQTT message on subscribed topic to create an echo effect */
+    publishParams.message.topic.qos = MQTT_QOS_0_AT_MOST_ONCE;
     publishParams.message.topic.topic.utf8 = (uint8_t *)"zephyr/test";
     publishParams.message.topic.topic.size = strlen(publishParams.message.topic.topic.utf8);
     publishParams.message.payload.data = (uint8_t *)"Hello world from zephyr!";
@@ -168,6 +179,11 @@ void main(void)
     publishParams.dup_flag = 0U;
     publishParams.retain_flag = 0U;
     mqtt_publish(&clientCtx, &publishParams);
+    /* Poll for MQTT packets */
+    fds[0].fd = clientCtx.transport.tcp.sock;
+    fds[0].events = ZSOCK_POLLIN;
+    zsock_poll(fds, 1, 500);
+    mqtt_input(&clientCtx);
     k_msleep(PUBLISH_DELAY_MS);
   }
 }
@@ -180,7 +196,7 @@ static void _button_pressed_cb(const struct device *dev, struct gpio_callback *c
   printk("Button pressed at %"PRIu32"\r\n", k_cycle_get_32());
 }
 
-static void _dhcpv4_handler(struct net_mgmt_event_callback *netMgmtEvtCb, uint32_t mgmtEvent, struct net_if *netIf)
+static void _dhcpv4_event_handler(struct net_mgmt_event_callback *netMgmtEvtCb, uint32_t mgmtEvent, struct net_if *netIf)
 {
   char buf[NET_IPV4_ADDR_LEN];
 
@@ -207,15 +223,66 @@ static void _dhcpv4_handler(struct net_mgmt_event_callback *netMgmtEvtCb, uint32
   }
 }
 
-static void _mqtt_evt_handler(struct mqtt_client *client, const struct mqtt_evt *evt)
+static void _mqtt_event_handler(struct mqtt_client *client, const struct mqtt_evt *event)
 {
-  switch(evt->type)
+  switch(event->type)
   {
   case MQTT_EVT_CONNACK:
     mqttConnected = true;
+    printk("\r\nMQTT connection established\r\n");
     break;
   case MQTT_EVT_DISCONNECT:
     mqttConnected = false;
+    printk("MQTT connection closed\r\n");
+    break;
+  case MQTT_EVT_PUBLISH:
+    mqttReceivedMessageLength = mqtt_read_publish_payload(&clientCtx,
+                                                          mqttReceivedMessage,
+                                                          sizeof(mqttReceivedMessage));
+    if(mqttReceivedMessageLength > 0)
+    {
+      mqttReceivedMessage[mqttReceivedMessageLength] = '\0';
+      printk("Received a published message:\r\n" \
+             "topic:   %s\r\n"                   \
+             "id:      %d\r\n"                   \
+             "qos:     %d\r\n"                   \
+             "length:  %d\r\n"                   \
+             "message: %s\r\n",
+             event->param.publish.message.topic.topic.utf8,
+             event->param.publish.message_id,
+             event->param.publish.message.topic.qos,
+             event->param.publish.message.payload.len,
+             mqttReceivedMessage);
+    }
+    else if(0 == mqttReceivedMessageLength)
+    {
+      printk("Received an empty publish message\r\n");
+    }
+    else
+    {
+      printk("Failed to read publish message (%d)\r\n", mqttReceivedMessageLength);
+    }
+    break;
+  case MQTT_EVT_PUBACK:
+    printk("MQTT message published successfully\r\n");
+    break;
+  case MQTT_EVT_PUBREC:
+    printk("Received confirmation for published message with QoS 2\r\n");
+    break;
+  case MQTT_EVT_PUBREL:
+    printk("Release of published message with QoS 2\r\n");
+    break;
+  case MQTT_EVT_PUBCOMP:
+    printk("Publish release message with QoS 2 confirmed\r\n");
+    break;
+  case MQTT_EVT_SUBACK:
+    printk("Subscribe request acknowledged\r\n");
+    break;
+  case MQTT_EVT_UNSUBACK:
+    printk("Unsubscribe request acknowledged\r\n");
+    break;
+  case MQTT_EVT_PINGRESP:
+    printk("Received ping response from server\r\n");
     break;
   default:
     break;
